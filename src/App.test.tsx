9fb2c78cfb6d86057e8f4b5de7af0e7d8894d413
FLT4613 <ft4613st@gmail.com>
2022-04-18 01:00:30 +0900

    improve step1's test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -7,16 +7,20 @@ describe("Step 1", () => {
         render(<App />)
         expect(screen.getAllByRole("button")).toHaveLength(5)
     })
-
-    test.each([
+    describe.each([
         ["coffee", "コーヒー", "480円"],
         ["tea", "お茶", "280円"],
         ["milk", "ミルク", "180円"],
         ["coke", "コーラ", "190円"],
         ["beer", "ビール", "580円"],
     ])("button %s exists with a label '%s %s'", (id, name, price) => {
-        render(<App />)
-        expect(screen.getByTestId(id)).toHaveTextContent(name)
-        expect(screen.getByTestId(id)).toHaveTextContent(price)
+        test("includes name", () => {
+            render(<App />)
+            expect(screen.getByTestId(id)).toHaveTextContent(name)
+        })
+        test("includes price", () => {
+            render(<App />)
+            expect(screen.getByTestId(id)).toHaveTextContent(price)
+        })
     })
 })
