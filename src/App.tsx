c526aabffd167632bd169951fdf392e5b20a1c1c
FLT4613 <ft4613st@gmail.com>
2022-04-19 12:14:56 +0900

    move the beverage data to outside of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,66 @@
-import React, { useState } from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 
+type Count = {
+    [k: string]: number
+}
+
+type Item = {
+    id: string,
+    displayName: string,
+    price: number
+}
+
+const items: Item[] = [{
+    id: "coffee",
+    displayName: "コーヒー",
+    price: 480
+}, {
+    id: "tea",
+    displayName: "お茶",
+    price: 280
+}, {
+    id: "milk",
+    displayName: "ミルク",
+    price: 180
+}, {
+    id: "coke",
+    displayName: "コーラ",
+    price: 190
+}, {
+    id: "beer",
+    displayName: "ビール",
+    price: 580
+}]
+
+
 function App () {
-    const [coffeeCount, setCoffeeCount] = useState(0)
-    const [teaCount, setTeaCount] = useState(0)
-    const [milkCount, setMilkCount] = useState(0)
-    const [coke, setCokeCount] = useState(0)
-    const [beerCount, setBeerCount] = useState(0)
+    const [itemsCount, setItemsCount] = useState<Count>(() => Object.fromEntries(items.map(v => [v.id, 0])))
+    const setCount = useCallback((key: string, value: number) => {
+        setItemsCount({
+            ...itemsCount,
+            [key]: value,
+        })
+    }, [itemsCount])
+
+    const totalItemsCount = useMemo(() => items.map(v => itemsCount[v.id]).reduce((prev, curr) => prev + curr), [itemsCount])
+    const totalPrice = useMemo(() => items.map(v => v.price * itemsCount[v.id]).reduce((prev, curr) => prev + curr), [itemsCount])
 
     return (
         <div className="App">
-            <span data-testid="count">{
-                coffeeCount + teaCount + milkCount + coke + beerCount
-            }</span>
-            <span data-testid="price">{
-                coffeeCount * 480 + teaCount * 280 + milkCount * 180 + coke * 190 + beerCount * 580
-            }</span>
-            <div data-testid={`coffee-count`}>{coffeeCount}</div>
-            <div data-testid={`tea-count`}>{teaCount}</div>
-            <div data-testid={`milk-count`}>{milkCount}</div>
-            <div data-testid={`coke-count`}>{coke}</div>
-            <div data-testid={`beer-count`}>{beerCount}</div>
-            <Counter id="coffee" name="コーヒー" price={480} setCount={setCoffeeCount} count={coffeeCount} />
-            <Counter id="tea" name="お茶" price={280} setCount={setTeaCount} count={teaCount} />
-            <Counter id="milk" name="ミルク" price={180} setCount={setMilkCount} count={milkCount} />
-            <Counter id="coke" name="コーラ" price={190} setCount={setCokeCount} count={coke} />
-            <Counter id="beer" name="ビール" price={580} setCount={setBeerCount} count={beerCount} />
+            <span data-testid="count">{totalItemsCount}</span>
+            <span data-testid="price">{totalPrice}</span>
+            <div data-testid={`coffee-count`}>{itemsCount["coffee"]}</div>
+            <div data-testid={`tea-count`}>{itemsCount["tea"]}</div>
+            <div data-testid={`milk-count`}>{itemsCount["milk"]}</div>
+            <div data-testid={`coke-count`}>{itemsCount["coke"]}</div>
+            <div data-testid={`beer-count`}>{itemsCount["beer"]}</div>
+
+            {
+                items.map(v => <Counter id={v.id} name={v.displayName} price={v.price} setCount={(value: number) => setCount(v.id, value)} count={itemsCount[v.id]} />)
+            }
+
         </div>
     );
 }
